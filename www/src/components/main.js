33b559aca228eaace6e7beda17c10bffa5009bca
The-Jackrabbit <lsm5fm@virginia.edu>
2017-05-15 18:45:50 -0400

    Fixed Scrollbar Issue on Contact Page
    Overflow in the y direction on the contact page has been fixed so that
    it will scroll properly

--- www/src/components/main.js
+++ www/src/components/main.js
@@ -27,7 +27,7 @@ class Main extends Component {
   render() {
     return (
       <Router>
-          <div>
+        <div className="test">
         <Route exact path="/" component={SplashScreen} />
         <Route path="/home" component={App} />
         </div>
